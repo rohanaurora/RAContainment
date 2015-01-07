516b268c7c6113dba6f07ffc510b19642c8993c1
Rohan Aurora <rohanaurora@gmail.com>
2015-01-06 23:29:34 -0600

    Add POC as new target.

---
+++ RAContainment/POC/POC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  POC
+//
+//  Created by Rohan Aurora on 1/6/15.
+//  Copyright (c) 2015 Rohan Aurora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
